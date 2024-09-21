0e76965b2dc5df33ecc9ea72cc78e8746c279891
Vanya et Aza <156308576+starvann@users.noreply.github.com>
2024-09-21 17:49:50 +0700

    Add files via upload

---
+++ TUGAS PYTHON/TUGAS 1 STRUKTUR DASAR PYTHON.py
@@ -0,0 +1,7 @@
+nama = 'Azaria Adila Putri'
+kelas = 'X PPLG 1'
+absen = '07' 
+print('Data diri')
+print('Nama:', nama)
+print('Kelas:', kelas)
+print('Nomor absen:', absen)
---
+++ TUGAS PYTHON/TUGAS2 STRUKTUR DASAR PYTHON.py
@@ -0,0 +1,4 @@
+print('Menghitung akar pangkat tiga')
+angka = input('Masukkan angka yang ingin dihitung:')
+hasil = int(angka)*int(angka)*int(angka)
+print("Hasil dari", angka, 'jika dipangkatkan tiga adalah', hasil)
