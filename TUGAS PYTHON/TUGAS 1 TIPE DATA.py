52231bfc73454af1cebd4553df1f147bbb1bc1cf
Vanya et Aza <156308576+starvann@users.noreply.github.com>
2024-09-21 18:05:56 +0700

    Add files via upload

---
+++ TUGAS PYTHON/TUGAS 1 TIPE DATA.py
@@ -0,0 +1,6 @@
+print('Konversi jarak dalam kilometer ke meter')
+kilo = input('Masukkan jarak dalam kilomoter')
+
+meter = int(kilo)*1000
+
+print('Hasil dari konversi jarak', kilo, 'km ke meter adalah', meter, 'm')
