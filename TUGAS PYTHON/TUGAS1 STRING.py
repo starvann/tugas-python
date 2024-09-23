4dcc20b0b3561ac2c3a98d45874157ae238bffc8
Vanya et Aza <156308576+starvann@users.noreply.github.com>
2024-09-23 08:59:59 +0700

    Add files via upload

---
+++ TUGAS PYTHON/TUGAS1 STRING.py
@@ -0,0 +1,13 @@
+nama = input('Masukkan nama anda')
+kelas = input('Masukkan kelas anda')
+absen = input('Masukkan nomor absen anda')
+jurusan = input('Masukkan jurusan yang anda ambil')
+h = 'Smk negeri 1 kandeman'
+j = 'Pengembangan Perangkat Lunak dan Game'
+
+print(j.center(50))
+print('Nama:', nama.upper())
+print('Kelas:', nama.upper())
+print('Absen:', nama.upper())
+print('Jurusan:', nama.upper())
+print(h.upper().center(50))
